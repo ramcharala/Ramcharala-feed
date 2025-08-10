cbbc8f43b7af63b2e3c27e16877e7dc381fb4b5d
ramcharala <remo.charala@gmail.com>
2025-08-10 23:32:27 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'scontent-*.cdninstagram.com',
-    },
-    {
-      protocol: 'https',
-      hostname: '*.fbcdn.net',
-    }
-  ]
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'scontent.cdninstagram.com',
+      'instagram.com',
+      'scontent-xx4-1.cdninstagram.com',
+      'scontent-lhr8-1.cdninstagram.com',
+      'scontent.fblr4-4.fna.fbcdn.net'
+    ],
+  },
+};
+
+module.exports = nextConfig;
